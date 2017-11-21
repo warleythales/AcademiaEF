69fc44532270f354a6b445c98fdc335c1083d9bb
wtsl_ <wtsl_@DESKTOP-70KD074>
2017-11-21 00:57:55 -0200

    Adicionando Array List de mensalidade e avaliação na classe aluno Criando classes DAO de Treino e Exercicio

--- src/java/bin/Aluno.java
+++ src/java/bin/Aluno.java
@@ -1,6 +1,13 @@
 package bin;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Date;
+import model.AlunoDAO;
+import model.Conexao;
+import model.FuncionarioDAO;
 
 public class Aluno {
 
@@ -122,23 +129,87 @@ public class Aluno {
         this.academia = academia;
     }
 
-    @Override
-    public String toString() {
-        return "Aluno{"
-                + "\n id=" + id
-                + "\n cpf=" + cpf
-                + "\n nome=" + nome
-                + "\n email=" + email
-                + "\n data_nasc=" + data_nasc
-                + "\n sexo=" + sexo
-                + "\n endereco=" + endereco
-                + "\n telefone=" + telefone
-                + "\n status=" + status
-                + "\n valor_mensalidade=" + valor_mensalidade
-                + "\n plano=" + plano
-                + "\n senha=" + senha + '}';
+    public ArrayList<Avaliacao> getListarAvaliacao() {
+        ArrayList<Avaliacao> listaA = new ArrayList<Avaliacao>();
+        try {
+            String sql = "SELECT id, biotipo, data_ava, idade, peso, ombro, braco_relaxado, braco_contraido, "
+                    + "antebraco, torax, panturrilha, cintura, abdomen, quadril, coxa, "
+                    + "validade_ava, id_aluno FROM avaliacao_fisica ";
+            Connection con = Conexao.conectar();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+            AlunoDAO daoAluno = new AlunoDAO();
+
+            while (rs.next()) {
+                Avaliacao a = new Avaliacao();
+                a.setId(rs.getInt("id"));
+                a.setBiotipo(rs.getString("biotipo"));
+                a.setData_ava(rs.getDate("data_ava"));
+                a.setIdade(rs.getInt("idade"));
+                a.setPeso(rs.getDouble("peso"));
+                a.setOmbro(rs.getDouble("ombro"));
+                a.setBraco_relaxado(rs.getDouble("braco_relaxado"));
+                a.setBraco_contraido(rs.getDouble("braco_contraido"));
+                a.setAntebraco(rs.getDouble("antebraco"));
+                a.setTorax(rs.getDouble("torax"));
+                a.setPanturrilha(rs.getDouble("panturrilha"));
+                a.setQuadril(rs.getDouble("quadril"));
+                a.setCoxa(rs.getDouble("coxa"));
+                a.setValidade_ava(rs.getDate("validade_ava"));
+
+                Aluno aluno = daoAluno.carregarPorId(rs.getInt("id_aluno"));
+                a.setAluno(aluno);
+
+                listaA.add(a);
+
+            }
+            System.out.println("Listar executado");
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println("AvaliacaoDAO::listar");
+            System.out.println(e.getMessage());
+        }
+        return listaA;
     }
-    //testes
-    //não sei
-    //testes 4
+
+    public ArrayList<Mensalidade> getListarMensalidade() {
+        ArrayList<Mensalidade> listaM = new ArrayList<Mensalidade>();
+        try {
+            String sql = "SELECT id, valor_pago, data_pag, data_venc, status,"
+                    + " id_aluno, id_funcionario FROM mensalidades";
+            Connection con = Conexao.conectar();
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(sql);
+
+            AlunoDAO daoAluno = new AlunoDAO();
+            FuncionarioDAO fdao = new FuncionarioDAO();
+            while (rs.next()) {
+                Mensalidade m = new Mensalidade();
+                m.setId(rs.getInt("id"));
+                m.setValor_pago(rs.getDouble("valor_pago"));
+                m.setData_pag(rs.getDate("data_pag"));
+                m.setData_venc(rs.getDate("data_venc"));
+                m.setStatus(rs.getString("status"));
+
+                Aluno aluno = daoAluno.carregarPorId(rs.getInt("id_aluno"));
+                m.setAluno(aluno);
+
+                Funcionario f = fdao.recuperarPorId(rs.getInt("id_funcionario"));
+                m.setFuncionario(f);
+
+                listaM.add(m);
+
+            }
+
+            System.out.println("MensalidadeDAO::getLista");
+            rs.close();
+            con.close();
+        } catch (Exception e) {
+            System.out.println("MensalidadeDAO::getLista");
+            System.out.println(e.getMessage());
+        }
+        return listaM;
+    }
+
 }
