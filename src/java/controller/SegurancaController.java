d37548bfc4ab2792009cab2917c2c103e2dbaad8
pc <pc@pc-PC>
2017-11-26 21:17:43 -0200

    Já enviei o Projeto Segue o que eu alterei: -Acrescentei o arquivo index.jsp ao projeto que estava sendo usado para o login. Agora o arquivo login chama-se login.jsp -O nome da Academia no site é onde está o link para a Página Incial. -Acrescentei a variavel Ativo para o Sitema, que faz referencia ao status do funcionario. E com isso acrescentei essa variavel ao Crud e Classe de controle de Funcionario.

--- src/java/controller/SegurancaController.java
+++ src/java/controller/SegurancaController.java
@@ -1,4 +1,3 @@
-
 package controller;
 
 import bin.Funcionario;
@@ -11,51 +10,47 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import model.FuncionarioDAO;
 
-
 public class SegurancaController extends HttpServlet {
-  
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
-         if (request.getParameter("logout") != null){
+
+        if (request.getParameter("logout") != null) {
             HttpSession sessao = request.getSession();
             sessao.removeAttribute("funcionario");
             response.sendRedirect("login.jsp");
         }
     }
 
-   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
         String cpf = request.getParameter("cpf");
         String senha = request.getParameter("senha");
         PrintWriter out = response.getWriter();
         FuncionarioDAO dao = new FuncionarioDAO();
         Funcionario funcionario = dao.pegaUm(cpf);
-        if ( funcionario == null ) {
+        if (funcionario == null) {
             out.print("<script>alert('Usuário ou Senha inválido.');location.href='login.jsp';</script>");
-        } else { 
-           if (funcionario.isAtivo()){
-            if (funcionario.getSenha().equals(senha)){
-                HttpSession sessao = request.getSession();
-                sessao.setAttribute("funcionario", funcionario);
-                response.sendRedirect("index.jsp");
+        } else {
+            if (funcionario.isAtivo()) {
+                if (funcionario.getSenha().equals(senha)) {
+                    HttpSession sessao = request.getSession();
+                    sessao.setAttribute("funcionario", funcionario);
+                    response.sendRedirect("index.jsp");
+                } else {
+                    out.print("<script>alert('Usuário ou Senha inválido kk.');location.href='login.jsp';</script>");
+                }
+
             } else {
-                out.print("<script>alert('Usuário ou Senha inválido kk.');location.href='login.jsp';</script>");
-            } 
-                    
-                    
-           }else{
-                out.print("<script>alert('Usuário bloqueado.');location.href='login.jsp';</script>");
-           }
-         
+                out.print("<script>alert('Usuário bloqueado contate o administrador.');location.href='login.jsp';</script>");
+            }
+
         }
     }
 
-    
     @Override
     public String getServletInfo() {
         return "Short description";
